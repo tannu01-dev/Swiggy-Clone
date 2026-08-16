4cdee92b15ad1e093bb017c2c7c1aa54ab4069b1
TANNU PAL <tannu.pal15072007@gmail.com>
2026-08-16 12:54:48 +0530

    Fix

--- src/pages/CreateResturant.jsx
+++ src/pages/CreateResturant.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./../Style/create.css";
+import "./../style/create.css";
 
 const CreateRestaurant = () => {
   const navigate = useNavigate();
